cc97857135ec4ba31bac2dcbf0cb06144b15b2d8
nguyenlinhns-arch <nguyenlinhns@gmail.com>
2026-08-14 17:49:50 +0700

    Publish local recruitment landing page config

---
+++ tuyen-tho-mo/tuyen-dung-dia-ban/config.js
@@ -0,0 +1,7 @@
+// Cấu hình triển khai.
+// Nếu sau này có Google Apps Script / webhook nhận lead, điền URL vào FORM_ENDPOINT.
+// Để trống: form sẽ tạo nội dung ứng tuyển và mở Zalo 0398696879.
+window.RECRUIT_CONFIG = {
+  FORM_ENDPOINT: "",
+  ZALO_PHONE: "0398696879"
+};
