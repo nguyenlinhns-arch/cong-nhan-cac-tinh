a2656799559343f1d040decc334a265991c04fa1
nguyenlinhns-arch <nguyenlinhns@gmail.com>
2026-08-23 09:23:13 +0700

    Bump cache cho loader video tỉnh tối ưu

--- tuyen-tho-mo/home-worker-journey.js
+++ tuyen-tho-mo/home-worker-journey.js
@@ -12,7 +12,7 @@
 
   if (!document.querySelector("script[data-home-province-reels-loader]")) {
     const provinceReels = document.createElement("script");
-    provinceReels.src = "/home-province-reels.js?v=20260822-3";
+    provinceReels.src = "/home-province-reels.js?v=20260823-1";
     provinceReels.defer = true;
     provinceReels.dataset.homeProvinceReelsLoader = "";
     document.head.append(provinceReels);
